d87546f5de3b402ec6cb885bde4fff2c2c69ca17
Stefan Taubert <stefan.taubert@posteo.de>
2021-06-01 12:12:02 +0200

    .

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name="speech_dataset_parser",
-    version="1.0.1",
-    url="https://github.com/stefantaubert/speech-dataset-parser.git",
-    author="Stefan Taubert",
-    author_email="contact@stefantaubert.com",
-    description="Parser for several speech datasets.",
-    packages=["speech_dataset_parser"],
-    install_requires=[
-        "numpy; python_version >= '3.6'",
-        "pandas>=1.1.4",
-        "python-dateutil>=2.8.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
-        "pytz>=2020.4",
-        "six>=1.15.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
-        "tqdm>=4.54.0",
-        "wget>=3.2",
-    ],
-)
+if __name__ == "__main__":
+  setuptools.setup()
